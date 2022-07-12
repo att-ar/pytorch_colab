db535b5a8d6900bb6557f88c1fe0a41857dd26f2
Attar Aziz <attar_a@outlook.com>
2022-07-12 15:13:50 -0400

    another commit

--- path_helper.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-import_path = os.path.abspath(os.path.join(os.getcwd(), ".."))
-
-if import_path not in sys.path:
-    sys.path.append(import_path)
--- stopinfo.py
+++
@@ -1,2 +0,0 @@
-from os import environ
-environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
